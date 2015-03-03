5227b311ba0099626dd9bdedb1cdc46218e2caae
Bradley J. Spaulding <brad.spaulding@gmail.com>
2015-03-02 20:39:42 -0800

    Initial Commit

---
+++ AutometryTests/AutometryTests.swift
@@ -0,0 +1,36 @@
+//
+//  AutometryTests.swift
+//  AutometryTests
+//
+//  Created by Bradley Spaulding on 3/2/15.
+//  Copyright (c) 2015 Motingo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class AutometryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
