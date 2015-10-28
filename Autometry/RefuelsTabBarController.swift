d7e63fe878fabdbd940a6d0ec390acf79577bf7f
Bradley Spaulding <brad.spaulding@gmail.com>
2015-10-28 14:24:11 -0700

    Using UIActivityViewController instead of MFMailComposeViewController.

--- Autometry/RefuelsTabBarController.swift
+++ Autometry/RefuelsTabBarController.swift
@@ -1,38 +1,30 @@
 import UIKit
 import MessageUI
 
-class RefuelsTabBarController : UITabBarController, MFMailComposeViewControllerDelegate {
+class RefuelsTabBarController : UITabBarController {
   let refuelStore = RefuelStore.sharedInstance
-  var picker : MFMailComposeViewController?
   let tintColor = UIColor(red: 231.0/255.0, green: 76.0/255.0, blue: 60.0/255.0, alpha: 1.0)
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+
     UITabBar.appearance().tintColor = tintColor
   }
-  
+
   @IBAction func export(sender: AnyObject) {
-    let picker = MFMailComposeViewController()
-    self.picker = picker
-    picker.view.tintColor = tintColor
-    picker.mailComposeDelegate = self
-    picker.setSubject("Autometry Data Export")
-    picker.setMessageBody("Hi!\n\nAttached is a spreadshet export of your data. Thanks for using Autometry!", isHTML: false)
-    
     let refuels = refuelStore.all()
     let csvString = RefuelCSVWrapper.wrapAll(refuels)
-    let csvData = csvString.dataUsingEncoding(NSUTF8StringEncoding)
-    picker.addAttachmentData(csvData!, mimeType:"text/csv", fileName:"AutometryExport.csv")
-    
-    presentViewController(picker, animated:true, completion:nil)
-  }
-  
-  func mailComposeController(controller: MFMailComposeViewController,
-    didFinishWithResult result: MFMailComposeResult,
-    error: NSError?) {
-      if let picker = self.picker {
-        picker.dismissViewControllerAnimated(true, completion:nil)
-      }
+
+    let docPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0]
+    let filePath = NSURL(fileURLWithPath: docPath).URLByAppendingPathComponent("AutometryDataExport.csv")
+    do {
+      try csvString.writeToURL(filePath, atomically: true, encoding: NSUTF8StringEncoding)
+
+      let activityViewController = UIActivityViewController(activityItems: [filePath], applicationActivities: nil)
+      activityViewController.setValue("Autometry Data Export", forKey: "subject")
+      presentViewController(activityViewController, animated: true, completion: {})
+    } catch {
+      print("writing to csv failed: \(error)")
+    }
   }
 }
