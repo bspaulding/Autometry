5bf2feb814fe4fdd6be86a09aea4fbfa04e1a101
Bradley Spaulding <brad.spaulding@gmail.com>
2016-04-02 19:31:12 -0700

    Update GoogleMapsAPI to use new "type" parameter, instead of older "types" parameter.

--- Autometry/GoogleMapsAPI.swift
+++ Autometry/GoogleMapsAPI.swift
@@ -7,7 +7,7 @@ class GoogleMapsAPI {
   }
   
   class func gasStationsNearby(latitude:Double, longitude:Double, callback:(AnyObject?) -> ()) {
-    let url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=\(GoogleMapsAPI.apiKey)&types=gas_station&rankby=distance&location=\(latitude),\(longitude)"
+    let url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=\(GoogleMapsAPI.apiKey)&type=gas_station&rankby=distance&location=\(latitude),\(longitude)"
     Alamofire.request(.GET, url).responseJSON {response in
       if let json = response.result.value {
         callback(json)
